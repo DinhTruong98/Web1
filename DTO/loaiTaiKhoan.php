411809f69a8a55958a536ad1436aa85aeef4ec02
friendlythinh2803 <friendlythinh2803@users.noreply.github.com>
2018-12-13 19:46:06 +0700

    them loai tai khoan DTO.

---
+++ DTO/loaiTaiKhoan.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nguyen Van Thinh
+ * Date: 12/13/2018
+ * Time: 7:43 PM
+ */
+
+class loaiTaiKhoan
+{
+    var $maLoaiTaiKhoan;
+    var $tenLoaiTaiKhoan;
+
+    public  function  __construct()
+    {
+        $this->maLoaiTaiKhoan= 0;
+        $this->tenLoaiTaiKhoan="";
+    }
+
+}
+?>
